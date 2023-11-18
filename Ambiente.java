b0fb8e1cd11cc165a280d7c69299995137971625
Luis <luisgustavost09@gmail.com>
2023-11-18 16:48:55 -0300

    Imports

--- Ambiente.java
+++ Ambiente.java
@@ -1,4 +1,8 @@
-import java.util.*;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Set;
 
 public class Ambiente {
 
